62aa2bb97a4082493099163ab0e4a692b463431d
lighthart <dlighthart@mesd.k12.or.us>
2015-04-03 10:42:44 -0700

    Delete type button

--- src/Mesd/LunchBundle/Controller/RestaurantController.php
+++ src/Mesd/LunchBundle/Controller/RestaurantController.php
@@ -25,18 +25,18 @@ class RestaurantController extends Controller
 
         $entities = $em->getRepository('MesdLunchBundle:Restaurant')->findAll();
 
+        $editForms = [];
         foreach($entities as $entityKey => $entityValue){
             $editForms[$entityValue->getId()] = $this->createEditForm($entityValue)->createView();
-
         }
 
         $entity = new Restaurant();
         $newForm   = $this->createCreateForm($entity)->createView();
-        
-       
+
+
         return $this->render('MesdLunchBundle:Restaurant:index.html.twig', array(
             'entities' => $entities,
-            'newForm'   => $newForm, 
+            'newForm'   => $newForm,
             'editForms' => $editForms,
         ));
     }
@@ -185,7 +185,7 @@ class RestaurantController extends Controller
 
         if ($editForm->isValid()) {
             $em->flush();
-            
+
             $request->getSession()->getFlashBag()->add('notice','Your changes were saved!');
             return $this->redirect($this->generateUrl('restaurant', array('id' => $id)));
 
--- src/Mesd/LunchBundle/Form/Extension/DeleteTypeExtension.php
+++ src/Mesd/LunchBundle/Form/Extension/DeleteTypeExtension.php
@@ -3,9 +3,31 @@
 namespace Mesd\LunchBundle\Form\Extension;
 
 use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class DeleteTypeExtension extends AbstractTypeExtension
 {
+
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        if (array_key_exists('delete', $options)) {
+            $view->vars['delete'] = $options['delete'];
+        }
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setOptional(['delete']);
+    }
+
     /**
      * Returns the name of the type being extended.
      *
